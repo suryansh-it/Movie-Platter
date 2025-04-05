c4dfb3651a04fbb355c0270f93647e5e3d353491
Suryansh Sharma <suryansharma09@gmail.com>
2025-04-05 11:18:01 +0530

    MovieCard component created

---
+++ Movie Platter Frontend/src/components/MovieCard.jsx
@@ -0,0 +1,23 @@
+function MovieCard({movie}){
+
+    function onfavouriteClick(){
+        alert("clicked")
+    }
+
+    return <div className="movie-card">
+            <div className="movie-poster">
+                <img src= {movie.url} alt= {movie.title}/>
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onCLick = {onfavouriteClick}>
+                        <p>dil</p>
+                    </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.released_date}</p>
+            </div>
+    </div>
+}
+
+export default MovieCard
