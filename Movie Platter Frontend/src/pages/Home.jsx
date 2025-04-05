bf89733669a481827c43e4954baa45a3ca56eeb2
Suryansh Sharma <suryansharma09@gmail.com>
2025-04-05 15:36:12 +0530

    integrating api

--- Movie Platter Frontend/src/pages/Home.jsx
+++ Movie Platter Frontend/src/pages/Home.jsx
@@ -1,16 +1,38 @@
 import MovieCard from "../components/MovieCard"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../css/Home.css"
+import { searchMovies, getPopularMovies } from "../services/api";
 
 function Home() {
 
     const [searchQuery, setSearchQuery] =useState("");
+    // storing movies in state so everytime we update movie list , it will automatically rerender the coponent for us
+    const [movies, setMovies]= useState([]);
+    const [error, setError]= useState(null);// to store error
+    const [loading, setLoading]= useState(true);//to store loading data
 
-    const movies= [
-        {id :1, title : "John Wick", release_date : "2020"},
-        {id :1, title : "Terminator", release_date : "1999"},
-        {id :1, title : "The Matrix", release_date : "1998"},
-    ]
+
+    useEffect(()=>{
+        const loadPopularMovies= async ()=>{
+            try {
+                const popularmovies = await getPopularMovies()
+                setMovies(popularmovies)
+            }catch(err){
+                console.log(err)
+                setError("Failed to load movies...")
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+        loadPopularMovies()
+    }, [])
+
+    // const movies= [
+    //     {id :1, title : "John Wick", release_date : "2020"},
+    //     {id :1, title : "Terminator", release_date : "1999"},
+    //     {id :1, title : "The Matrix", release_date : "1998"},
+    // ]
 
     const handleSearch = (e) => {
         e.preventDefault();
