3958b15b2dcd563eedf141a90e3227a931917792
Suryansh Sharma <suryansharma09@gmail.com>
2025-04-05 12:35:55 +0530

    used usestate

--- Movie Platter Frontend/src/pages/Home.jsx
+++ Movie Platter Frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react";
 
 function Home() {
+
+    const [searchQuery, setSearchQuery] =useState("");
 
     const movies= [
         {id :1, title : "John Wick", release_date : "2020"},
@@ -8,15 +11,23 @@ function Home() {
         {id :1, title : "The Matrix", release_date : "1998"},
     ]
 
-    const handleSearch = () => {};
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery)
+        setSearchQuery("")
+    };
 
     return(
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
-                <input type="text" placeholder="Search movies..." className="search-input" />
+                <input type="text" placeholder="Search movies..." className="search-input" 
+                value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+
+                <button type="submit" className="search-button">Search</button>
             </form>
-            <div className="movies-grid">
-                {movies.map((movie) => (
+            <div className="movies-grid"> 
+            {/* tosearch movies according to query */}
+                {movies.map((movie) => movie.title.toLowerCase().startsWith(searchQuery) && (   
                     <MovieCard movie={movie} key= {movie.id}/> //display this for every singlr elemetn 
                 ))}
             </div>
