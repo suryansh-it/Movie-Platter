4095f3090c2b816a7f54b5ea9eac82dc96dfc68b
Suryansh Sharma <suryansharma09@gmail.com>
2025-04-05 11:41:35 +0530

    MovieCard component displayed

--- Movie Platter Frontend/src/App.jsx
+++ Movie Platter Frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import MovieCard from './components/MovieCard'
 import './App.css'
 
 function App() {
-  
+  //conditional rendering
+  const movieNum =1;
+
 
   return (
     <>
-      
+    { movieNum ===1 ? (
+    <MovieCard movie={{title: "Movie 1", release_date:"2024"}}></MovieCard>
+  ) :(
+    <MovieCard movie={{title: "Movie 2", release_date:"2020"}}></MovieCard>)}
     </>
-  )
+  );
 }
 
 export default App
